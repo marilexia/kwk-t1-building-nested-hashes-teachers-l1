ae8cea949dbd076e71986d640fea6217d2ba4e16
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-07 21:46:33 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
       :hero => {
         #your key/value pairs here
         :name => "Romeo",
-        :age => 15,
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
@@ -23,12 +23,12 @@ def third_challenge
       :patriarch => {
         #your key/value pairs here
         :name => "Lord Capulet",
-        :age => 50
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
         :name => "Lady Capulet",
-        :age => 51
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
